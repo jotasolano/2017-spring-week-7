2814215d0e95b0ea4f92e9601e2b04df03fee335
siqister <siqi.zhu@gmail.com>
2017-02-24 10:53:51 -0500

    first commit

--- public/zoom/app.js
+++ public/zoom/app.js
@@ -1,10 +1,78 @@
 console.log(d3);
 
-var dispatcher = d3.dispatch('update');
+/*Example 1: Basic zoom usage
+*/
+var m = {t:20,r:20,b:20,l:20},
+	w = d3.select('#plot1').node().clientWidth - m.l - m.r,
+	h = d3.select('#plot1').node().clientHeight - m.t - m.b;
+var svg = d3.select('#plot1').append('svg')
+	.attr('width',w + m.l + m.r)
+	.attr('height',h+ m.t + m.b);
+var plot1 = svg.append('g')
+	.attr('transform','translate('+m.l+','+m.t+')');
+
+for(var y = 0; y<=h; y+= h/10){
+	for(var x = 0; x<=w; x+= w/30){
+		plot1.append('circle')
+			.attr('cx',x)
+			.attr('cy',y)
+			.attr('r',3)
+			.style('stroke','black')
+			.style('stroke-width','1px')
+			.style('fill','none');
+	}
+}
+var rect = svg.append('rect')
+	.attr('width',w)
+	.attr('height',h)
+	.attr('transform','translate('+m.l+','+m.t+')')
+	.style('fill','none')
+	.style('pointer-events','all');
+
+//Part 1: define zoom behavior
+var zoom = d3.zoom()
+	.scaleExtent([.5,3]);
+
+
+//Part 2: event listeners
+zoom
+	.on('start',function(){
+		console.log(d3.event.target);
+		console.log(d3.event.type);
+		console.log(d3.event.transform);
+		console.log(d3.event.sourceEvent);
+		console.log('---');
+	}) 
+	.on('zoom',function(){
+		var transform = d3.event.transform;
+		plot1
+			.attr('transform', 'translate('+ (transform.x+m.l) + ',' + (transform.y+m.t) + ')scale(' + transform.k +')')
+			.selectAll('circle')
+			.style('stroke-width',1/transform.k+'px');
+	})
+	.on('end',function(){
+		console.log(d3.event.target);
+		console.log(d3.event.type);
+		console.log(d3.event.transform);
+		console.log(d3.event.sourceEvent);
+		console.log('---');
+	});
+
+//Part 3: apply zoom behavior
+rect.call(zoom);
+
+//Part 4: setting zoom transform programmatically
+d3.select('body').on('click',function(){
+	rect.transition().call(zoom.transform, d3.zoomIdentity);
+});
+
+
+
+
 
 d3.queue()
-	.defer(d3.csv,'./data/hubway_trips_reduced.csv',parseTrips)
-	.defer(d3.csv,'./data/hubway_stations.csv',parseStations)
+	.defer(d3.csv,'../data/hubway_trips_reduced.csv',parseTrips)
+	.defer(d3.csv,'../data/hubway_stations.csv',parseStations)
 	.await(dataLoaded);
 
 function dataLoaded(err,trips,stations){
@@ -15,49 +83,11 @@ function dataLoaded(err,trips,stations){
 		tripsByStartStation = cf.dimension(function(d){return d.startStn}),
 		tripsByEndStation = cf.dimension(function(d){return d.endStn});
 
-	//Time series module
-	var timeseries = Timeseries()
-		.on('timerange:select',function(range){
-			console.log('App:time range:filter');
-			console.log(range);
-			tripsByStartTime.filter(range);
-			dispatcher.call('update');
-		});
-	//Pie chart module: showing user type breakdown
-	var piechartByUserType = Piechart().value(function(d){return d.userType});
-	//Pie chart module: showing user gender breakdown
-	var piechartByUserGender = Piechart().value(function(d){return d.userGender});
+	//Create a map module
+	var map = Map()
+		.height(600);
+	d3.select('#plot2').datum(stations).call(map);
 
-	//UI module
-	var startStationList = StationList()
-		.on('station:select',function(id){
-			console.log('App:start station:select')
-			console.log(id);
-			tripsByStartStation.filter(id);
-			dispatcher.call('update');
-		});
-	var endStationList = StationList()
-		.on('station:select',function(id){
-			console.log('App:end station:select')
-			console.log(id);
-			tripsByEndStation.filter(id);
-			dispatcher.call('update');
-		});
-	d3.select('#start-station').datum(stations).call(startStationList);
-	d3.select('#end-station').datum(stations).call(endStationList);
-
-	//Listen to global dispatcher events
-	dispatcher.on('update',update);
-	update();
-
-	function update(){
-		d3.select('#plot1').datum(tripsByStartTime.top(Infinity))
-			.call(timeseries);
-		d3.select('#plot2').datum(tripsByStartTime.top(Infinity))
-			.call(piechartByUserType);
-		d3.select('#plot3').datum(tripsByStartTime.top(Infinity).filter(function(d){return d.userGender}))
-			.call(piechartByUserGender);
-	}
 }
 
 function parseTrips(d){
---
+++ public/zoom/modules/Map.js
@@ -0,0 +1,85 @@
+function Map(){
+	var projection = d3.geoMercator();
+	var m = {t:20,r:20,b:20,l:20},
+		w, h, W=800, H=600;
+	var zoom = d3.zoom();
+	var plot;
+
+	var exports = function(div){
+		//Reset width and height and projection
+		W = Math.max(div.node().clientWidth, W);
+		H = Math.max(div.node().clientHeight, H);
+		w = W - m.l - m.r;
+		h = H - m.t - m.b;
+
+		projection
+			.center([-71.091903,42.350037])
+			.translate([w/2, h/2])
+			.scale(410000);
+
+		div.style('width',W+'px')
+			.style('height',H+'px');
+
+		var datum = div.datum() || [];
+
+		//Zoom behavior
+		zoom
+			.scaleExtent([.8,1.5])
+			.on('zoom',zoomed);
+
+		//Append DOM
+		var svg = div.selectAll('svg').data([1]);
+		var svgEnter = svg.enter().append('svg');
+		svgEnter.append('rect')
+			.attr('x',0)
+			.attr('y',0)
+			.attr('width',W)
+			.attr('height',H)
+			.style('fill','none')
+			.style('pointer-events','all')
+			.call(zoom);
+		var plotEnter = svgEnter.append('g').attr('class','map');
+
+		plot = svgEnter.merge(svg)
+			.attr('width',W)
+			.attr('height',H)
+			.select('.map')
+			.attr('transform','translate('+m.l+','+m.t+')');
+
+		//Draw dots
+		var stations = plot.selectAll('.station')
+			.data(datum);
+		var stationsEnter = stations.enter()
+			.append('g').attr('class','station');
+		stationsEnter.append('circle').attr('r',5)
+			.style('fill','none').style('stroke','red').style('stroke-width','2px');
+		stationsEnter.append('circle').attr('r',2)
+			.style('fill','red');
+		stations.merge(stationsEnter)
+			.attr('transform',function(d){
+				var xy = projection(d.lngLat);
+				return 'translate('+xy[0]+','+xy[1]+')';
+			});
+		stations.exit().remove();
+
+	}
+
+	function zoomed(){
+		var transform = d3.event.transform;
+		plot.attr('transform', 'translate('+(transform.x+m.l)+','+(transform.y+m.t)+')scale('+transform.k+')');
+	}
+
+	exports.width = function(_){
+		if(!arguments.length) return W;
+		W = _;
+		return this;
+	}
+
+	exports.height = function(_){
+		if(!arguments.length) return H;
+		H = _;
+		return this;
+	}
+
+	return exports;
+}
